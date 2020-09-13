c9ca74cbbe0b03093c1ce577451e05d9645ce2c7
南风北巷 <1575374226@qq.com>
2020-09-13 22:28:12 +0800

    2020-09-13    搭建项目整体框架，并完成底部路由切换

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+// 引入iconfont资源
+import './assets/iconfont/iconfont.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
